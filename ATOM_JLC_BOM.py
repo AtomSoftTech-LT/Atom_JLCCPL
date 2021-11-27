a414eae63a2c239683ac02d234a5c77e91587d27
Jason Lopez <atomsoft@gmail.com>
2021-11-26 20:42:32 -0500

    added BOM

---
+++ ATOM_JLC_BOM.py
@@ -0,0 +1,41 @@
+#EXPORT BOM in EAGLECAD
+#CHOOSE VALUES and CSV
+
+import sys
+
+
+if len(sys.argv) < 2:
+    print('No file to parse, use "' + str(sys.argv[0]) + ' filename.csv"')
+    quit()
+
+
+myFilename = str(sys.argv[1])
+
+file1 = open(myFilename, 'r')
+Lines = file1.readlines()
+ 
+count = 0
+# Strips the newline character
+myOutput = ""
+
+for line in Lines:
+    count += 1
+    #print("Line{}: {}".format(count, line.strip()))
+    if count > 3 :
+        myLine = line.strip()
+        x = myLine.split('"')
+
+        #newLine = x[3] + ", " + x[7] + ", " + x[9]
+        if x[3] != "DNP" :
+            x[9] = x[9].replace(",","")
+            myOutput += x[3] + ', ' + x[9] + ', ' + x[7] + " \n"
+            
+            print(myOutput)
+            
+print(myOutput)
+myOutputFilename = myFilename[0:-4:] + "_bom.csv"
+
+f = open(myOutputFilename, "w")
+f.write("Comment, Designator, Footprint, JLCPCB Part #\n")
+f.write(myOutput)
+f.close()
